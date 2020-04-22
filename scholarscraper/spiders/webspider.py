2913f731458f6dc4174c3243d5815445a21a36dc
Jaskaran Singh <jaskarannagi19@gmail.com>
2020-04-22 14:02:41 +0530

    Function to crawl Author url

--- scholarscraper/spiders/webspider.py
+++ scholarscraper/spiders/webspider.py
@@ -4,14 +4,20 @@
 # your spiders.
 
 import scrapy
-
+from scrapy.http import Request
 
 class WebSpider(scrapy.Spider):
     name = "web"
     start_urls = ['https://scholar.google.com/']
+    BASE_URL = 'https://scholar.google.com'
 
     def parse(self, response):
         yield scrapy.FormRequest.from_response(response,formxpath="//form[@id='gs_hdr_frm']",formdata={'q':'G. Brewka'},callback=self.parse_caseStatus)
 
     def parse_caseStatus(self,response):
-        response.xpath('//h4[@class="gs_rt2"]/a/text()').get()
+        print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+        author_url = response.xpath('//h4[@class="gs_rt2"]/a/@href').get()
+        print(response.xpath('//h4[@class="gs_rt2"]/a/@href').get())
+        yield Request(url=self.BASE_URL+author_url, callback=self.author_profile)
+
+
