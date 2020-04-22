aecb720ef0246328fe312910a5bdfecd7cbe3e71
Jaskaran Singh <jaskarannagi19@gmail.com>
2020-04-22 14:19:21 +0530

    H index extracted of given author in arguments

--- scholarscraper/spiders/webspider.py
+++ scholarscraper/spiders/webspider.py
@@ -12,13 +12,13 @@ class WebSpider(scrapy.Spider):
     BASE_URL = 'https://scholar.google.com'
 
     def parse(self, response):
-        yield scrapy.FormRequest.from_response(response,formxpath="//form[@id='gs_hdr_frm']",formdata={'q':'G. Brewka'},callback=self.parse_caseStatus)
+        yield scrapy.FormRequest.from_response(response,formxpath="//form[@id='gs_hdr_frm']",formdata={'q':self.author},callback=self.parse_caseStatus)
 
     def parse_caseStatus(self,response):
         author_url = response.xpath('//h4[@class="gs_rt2"]/a/@href').get()
-        #print(response.xpath('//h4[@class="gs_rt2"]/a/@href').get())
+        print(response.xpath('//h4[@class="gs_rt2"]/a/@href').get())
         yield Request(url=self.BASE_URL+author_url, callback=self.author_profile)
     def author_profile(self,response):
-        #print(response.xpath('//table[@id="gsc_rsb_st"]//tr[2]//td[2]/text()').get())
+        print(response.xpath('//table[@id="gsc_rsb_st"]//tr[2]//td[2]/text()').get())
         return response.xpath('//table[@id="gsc_rsb_st"]//tr[2]//td[2]/text()').get()
 
