d8a97d3e29e29ffb1e35814b28c195e48ca01869
Jaskaran Singh <jaskarannagi19@gmail.com>
2020-04-22 13:55:43 +0530

    Extracting Author profile link

--- scholarscraper/spiders/webspider.py
+++ scholarscraper/spiders/webspider.py
@@ -14,4 +14,4 @@ class WebSpider(scrapy.Spider):
         yield scrapy.FormRequest.from_response(response,formxpath="//form[@id='gs_hdr_frm']",formdata={'q':'G. Brewka'},callback=self.parse_caseStatus)
 
     def parse_caseStatus(self,response):
-        print(response.text)
+        response.xpath('//h4[@class="gs_rt2"]/a/text()').get()
